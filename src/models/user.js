17f619a83dfc6db1e89aa263f4d153e099d38ec8
Mauryavishal18 <2k23.it2313958@gmail.com>
2025-12-07 01:19:46 +0530

    validation using Validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator=require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -10,14 +11,24 @@ const userSchema = new mongoose.Schema({
     },
     emailId: {
         type: String,
-        lowercase:true,
         required: true,
         unique: true,
         trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address:" + value);
+            }
+
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Invalid email address:" + value);
+            }
+        }
     },
     age: {
         type: Number,
