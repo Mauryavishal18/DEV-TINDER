deb4d2185ecb5d633be43f6f82f88d98ac621241
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-02-19 16:43:15 +0530

    Creating Login Setup

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
+  const [EmailId, setEmailId] = useState("vishal@test.com");
+  const [Password, setPassword] = useState("Test@1234");
+
+  const handleLogin = async () => {
+  try {
+    const res = await axios.post("http://localhost:3000/login", {
+      emailId: EmailId,
+      password: Password,
+    });
+
+    console.log(res.data);
+    alert("Login Successful 🚀");
+
+  } catch (err) {
+    console.error(err.response?.data);
+    alert("Login Failed ❌");
+  }
+};
+
+
   return (
-    <div className="text-center text-2xl">
-      Login Page
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+
+          <label className="form-control w-full max-w-xs my-2">
+            <span className="label-text">Email ID</span>
+            <input
+              type="email"
+              value={EmailId}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </label>
+
+          <label className="form-control w-full max-w-xs my-2">
+            <span className="label-text">Password</span>
+            <input
+              type="password"
+              value={Password}
+              className="input input-bordered w-full max-w-xs"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 };
