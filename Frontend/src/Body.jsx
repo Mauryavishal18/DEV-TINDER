387fd19f30965a0755e0143b7f8d04c432bc5461
Mauryavishal18 <2k23.it2313958@gmail.com>
2026-02-21 00:55:20 +0530

    UI UPDATION

--- Frontend/src/Body.jsx
+++ Frontend/src/Body.jsx
@@ -1,19 +1,39 @@
-import Navbar from './Navbar';
-import { Outlet } from 'react-router-dom';
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(BASE_URL + "/profile", {
+          withCredentials: true,
+        });
+
+        dispatch(addUser(res.data));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchUser();
+  }, [dispatch]);
+
   return (
     <div className="flex flex-col min-h-screen">
-      
       <Navbar />
-
       <div className="grow p-5">
         <Outlet />
       </div>
-
       <Footer />
-
     </div>
   );
 };
